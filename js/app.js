19975a83a6920233b226942408db7ac2d0ea5ea7
asif-iqbal-munna <asifiqbalmunna1318@gmail.com>
2021-08-21 01:37:23 +0600

    macbook pro project is completed

--- js/app.js
+++ js/app.js
@@ -8,32 +8,73 @@ function getMemoryCost(costOf,costing){
 }
 
 
+// updating total cost of all costings in the function below
+function totalAmount(priceOf){
+    // debugger;
+    const price = document.getElementById(priceOf + '-cost');
+    const priceAmount = parseInt(price.innerText);
+    return priceAmount;
+}
+
+
+// adding all the amount of costings
+function getTotalPrice(){
+    const totalPrice = totalAmount('best') + totalAmount('memory') + totalAmount('storage') + totalAmount('delivery');
+    document.getElementById('total-price').innerText = totalPrice;
+    document.getElementById('total').innerText = totalPrice;
+    return totalPrice;
+}
+
+
+// updating total with or without promo code matching 
+function getPromoCode(){
+    const promoCodeField = document.getElementById('promo-code');
+    const promoCodeValue = (promoCodeField.value).toLowerCase();
+    const totalPrice = getTotalPrice();
+    if(promoCodeValue === "stevekaku"){
+        const discount = totalPrice / 5;
+        const total = totalPrice - discount;
+        document.getElementById('total').innerText = total;
+    }
+    promoCodeField.value = '';
+}
+
 // handling memory cost buttons
 document.getElementById('memory-8gb').addEventListener('click', function(){
     getMemoryCost('memory',0);
-
+    getTotalPrice();
 })
 document.getElementById('memory-16gb').addEventListener('click', function(){
     // debugger;
     getMemoryCost('memory',180);
+    getTotalPrice();
 })
 
 // handling storage cost buttons
 document.getElementById('storage-256gb').addEventListener('click', function(){
     getMemoryCost('storage',0);
+    getTotalPrice();
 })
 document.getElementById('storage-512gb').addEventListener('click', function(){
     getMemoryCost('storage',100);
+    getTotalPrice();
 })
 document.getElementById('storage-1tb').addEventListener('click', function(){
     getMemoryCost('storage',180);
+    getTotalPrice();
 })
 
 
 // handling delivery cost buttons
 document.getElementById('free-delivery').addEventListener('click', function(){
     getMemoryCost('delivery',0);
+    getTotalPrice();
 })
 document.getElementById('charged-delivery').addEventListener('click', function(){
     getMemoryCost('delivery',20);
+    getTotalPrice();
+})
+
+document.getElementById('PC-apply-btn').addEventListener('click', function(){
+    getPromoCode();
 })
